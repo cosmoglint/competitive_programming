7528f624b57d6a91d95cbbf4d505d4be12f9c1e6
thedoodler <ramprakashsmail@gmail.com>
2020-09-18 10:42:16 +0530

    removed unwanted files

--- max_subarray.cpp
+++ max_subarray.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
 
+using namespace std
+
+int main(){
+  int arr [8] = {-1,2,4,-3,5,2,-5,2};
+  int sums = 0;
+  int ans = 0;
+  for (int k = 0; k < n ; k++){
+    sums = max(array[k],sum+array[k]);
+    best = mx(best, sums);
+  }
+}
