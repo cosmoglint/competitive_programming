194385180e25849047ac9891e9e8839fd09099b6
thedoodler <ramprakashsmail@gmail.com>
2020-09-18 12:19:23 +0530

    working done

--- max_subarray.cpp
+++ max_subarray.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 
-using namespace std
+using namespace std;
 
 int main(){
-  int arr [8] = {-1,2,4,-3,5,2,-5,2};
+  int arr [8] = {-1,2,4,3,5,2,-5,2};
   int sums = 0;
   int ans = 0;
-  for (int k = 0; k < n ; k++){
-    sums = max(array[k],sum+array[k]);
-    best = mx(best, sums);
+  for (int k = 0; k < sizeof(arr)/sizeof(arr[0]) ; k++){
+    sums = max(arr[k],sums+arr[k]);
+    ans = max(ans, sums);
   }
+
+  cout << ans;
 }
