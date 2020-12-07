2fbaa9dd66a628b367c2be6eaffdd5d532e5ed7e
thedoodler <ramprakashsmail@gmail.com>
2020-12-07 14:40:54 +0530

    added set data structue

--- 5_data_structures.cpp
+++ 5_data_structures.cpp
@@ -5,6 +5,8 @@
 using namespace std;
 
 int main(){
+
+  //vectors
     vector<int> v;
     v.push_back(3);
     v.push_back(5);
@@ -17,8 +19,21 @@ int main(){
     for (int i=0; i < v.size(); i++){  // iterating the vector
       cout << v[i] << " ";
     }
-
+    cout << "\n";
     for (auto x: v){
       cout << x << " ";  //this is another way to iterate
     }
+    cout << "\n";
+
+
+    //sets
+    set<int> s;
+    s.insert(3);
+    s.insert(5);
+    s.insert(2);
+    s.insert(2);
+    cout << s.count(2) << "\n"; // prints 1 because sets cant have duplicates
+    cout << s.count(4) << "\n"; // prints 0 because 4 is not present
+    s.erase(5);
+    s.size(); // 2
 }
