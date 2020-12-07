1f2ade8f27643a6364d8f4d0e48637eb1822d2e9
thedoodler <ramprakashsmail@gmail.com>
2020-12-07 15:27:12 +0530

    added shuffle in iterators

--- 6_iterators.cpp
+++ 6_iterators.cpp
@@ -1,7 +1,19 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <random>
+#include <vector>
 
 using namespace std;
 
 int main(){
   vector<int> v = {1,3,5,2,3,4,7};
+  unsigned seed = 0;
+
+  sort(v.begin(),v.end());
+  reverse(v.begin(),v.end());
+  shuffle(v.begin(),v.end(), default_random_engine(seed));  // this is part of algorithm module
+
+  for (auto i=v.begin(); i<v.end(); i++){
+    cout << *i << " ";
+  }
 }
