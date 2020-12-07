a4a43263e2ccc32efdda7433a0d582916b48cb69
thedoodler <ramprakashsmail@gmail.com>
2020-12-07 13:06:49 +0530

    vector operations

--- 5_data_structures.cpp
+++ 5_data_structures.cpp
@@ -6,4 +6,17 @@ using namespace std;
 int main(){
     vector<int> v;
     v.push_back(3);
+    v.push_back(5);
+    v.push_back(2);
+    cout << v.back() << "\n"; // prints last element
+    cout << v[0] << "\n"; // prints first element
+    cout << *v.begin() << "\n"; // this also prints the first number but with pointer
+
+    for (int i=0; i < v.size(); i++){  // iterating the vector
+      cout << v[i] << " ";
+    }
+
+    for (auto x: v){
+      cout << x << " ";  //this is another way to iterate
+    }
 }
