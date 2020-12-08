362ddc366b0174dfb07e372d7461429bacdfd2ba
thedoodler <ramprakashsmail@gmail.com>
2020-12-08 13:44:41 +0530

    added generating subsets

--- 2_methods/1_generating_subsets.cpp
+++ 2_methods/1_generating_subsets.cpp
@@ -3,28 +3,33 @@
 
 using namespace std;
 
-void printer(vector v){
+
+vector<int> subset;
+int n = 3;
+
+void printer(vector<int> v){
   for (int i=0; i<v.size(); i++){
     cout << v[i] << " ";
   }
-  "\n"
-  return
+  cout << "\n";
+  return;
 }
 
-void search(int k, vector subset){
+void search(int k){
   if (k==n){
     printer(subset);
   }
   else{
-    search(k+1,subset);
+    search(k+1);
     subset.push_back(k);
-    search(k+1,subset);
-    subset.pop();
+    search(k+1);
+    subset.pop_back();
   }
+  return;
 }
 
 
 int main(){
   vector<int> the_v = {0,1,2};
-  search(0,)
+  search(0);
 }
